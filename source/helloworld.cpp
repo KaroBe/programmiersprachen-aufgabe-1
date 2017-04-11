a91a1a1752c936ac4f4533f88fd64008a03c8683
Karoline Brehm <karoline.engel.brehm@uni-weimar.de>
2017-04-11 19:23:36 +0200

    CMake Dateien, Lösungsblatt, kgv aufgabe

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,7 +1,24 @@
 # include <iostream>
 
-int main ()
-{
-std :: cout << "Hello , World !\n";
+int main (){
+
+
+
+/*
+bool foundit = false;
+int div = 20;
+
+
+while(foundit == false){
+	foundit = true;
+	for(int i = 1; i <=20; ++i){
+			if (div % i != 0){
+			foundit = false;
+   		}
+	}
+	div = div + 2;
+}
+div = div-2;
+*/
 return 0;
 }
