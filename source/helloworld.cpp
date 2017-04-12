c3bb73d20589c1f089a886551678ee99933de80e
Karoline Brehm <karoline.engel.brehm@uni-weimar.de>
2017-04-12 16:08:41 +0200

    TEST Typkonvertierung ergänzt, Compilierte Dateinen Aufg1.4

--- source/helloworld.cpp
+++ source/helloworld.cpp
@@ -1,10 +1,21 @@
 # include <iostream>
 
+// TEST Verhalten Typkonvertierung bei Berechnungen mit unterschiedlichen Typen
 int main (){
 
+int two = 2;
+double e = 3.5/two;
+std::cout << "int / double 3.5/2 : " << e << "\n";
 
 
-/*
+double g = 3/two;
+std::cout << "int? / int 3 / 2 : " << g << "\n";
+
+double f = two/3.5;
+std::cout << "double / int 2 / 3.5 : " << f << "\n";
+
+// BRUTE FORCE KGV Berechnug 1-20
+
 bool foundit = false;
 int div = 20;
 
@@ -16,9 +27,9 @@ while(foundit == false){
 			foundit = false;
    		}
 	}
-	div = div + 2;
+	div = div + 20;
 }
-div = div-2;
-*/
+div = div-20;
+
 return 0;
 }
