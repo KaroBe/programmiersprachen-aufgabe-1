cad53ea55f8174bfd2fc9107446a26fa7d684bfe
Karoline Brehm <karoline.engel.brehm@uni-weimar.de>
2017-04-20 11:56:53 +0200

    binomial funktion hinzugefügt

---
+++ build/source/CMakeFiles/tests_basic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests_basic.dir/tests_basic.cpp.o"
+  "tests_basic.pdb"
+  "tests_basic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests_basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
